2a3b1bbeab84b06e90e2ccaebf0be25c8264675e
Zviki_Zaks <zvikizax@gmail.com>
2022-05-10 11:20:44 +0300

    fix images linls

--- js/views/about-page.cmp.js
+++ js/views/about-page.cmp.js
@@ -11,7 +11,7 @@ export default {
         <section class="about-page app-layout">
             <h1>Miss-Book</h1>
             <p>This is my first VUE app</p>
-            <img src="/imgs/logo.svg">
+            <img src="imgs/logo.svg">
             <nav class="about-nav">
                 <router-link to="/about/thanks">Thanks</router-link>
             </nav>
